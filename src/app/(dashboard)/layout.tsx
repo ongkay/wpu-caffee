ecec77f949d5e6ae5815690b9dd49cbfc7092fa6
ongkay <bisnismilyarandollar@gmail.com>
2025-08-03 12:07:18 +0700

    feat(dashboard): add Breadcrumb and user

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import { DarkmodeToggle } from '@/components/common/darkmode-toggle';
 import { Separator } from '@/components/ui/separator';
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { ReactNode } from 'react';
+import DashboardBreadcrumb from './_components/dashboard-breadcrumb';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
@@ -16,6 +17,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
               orientation="vertical"
               className="mr-2 data-[orientation=vertical]:h-4"
             />
+            <DashboardBreadcrumb />
           </div>
           <div className="px-4">
             <DarkmodeToggle />
