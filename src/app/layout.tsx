1ff168e18a16b88721a0b5de781b6947aa57343f
ongkay <bisnismilyarandollar@gmail.com>
2025-08-03 15:14:24 +0700

    feat(dashboard): add Tanstack-React-Query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from '@/providers/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
 import AuthStoreProvider from '@/providers/auth-store-provider';
 import { cookies } from 'next/headers';
+import ReactQueryProvider from '@/providers/react-query-provider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,17 +26,19 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <AuthStoreProvider profile={profile}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-            <Toaster />
-          </ThemeProvider>
-        </AuthStoreProvider>
+        <ReactQueryProvider>
+          <AuthStoreProvider profile={profile}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </AuthStoreProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   );
